21de9a9561a14cae59fc78bdea71f60a3b54e339
aij0 <new@email>
2021-10-16 13:59:04 +0300

    Update tutor component with details and styling
    Added basic displaying of data from github database.
    Next TODO remove hardcoded userdata

--- my-app/src/components/tutor.js
+++ my-app/src/components/tutor.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./tutor.css";
 
+/* const tutor = {
+    name: "First Tutor",
+    price: 100,
+}
+<Tutor tutor={tutor} /> */
+
 class Tutor extends React.Component{
     render(){
+        const title = this.props.tutor.subject + " - " + this.props.tutor.price + this.props.tutor.priceCurrency
+        + " - " + this.props.tutor.name;
+
+        const style = {
+            backgroundImage: `url('${this.props.tutor.imageUrl}')`
+        };
+
         return (
             <div className="tutor">
-                <div className="tutor-picture"></div>
-                <div className="tutor-title"></div>
+                <div className="tutor-picture" style={style}></div>
+                <div className="tutor-title">
+                    {title}
+                </div>
             </div>
         );
     }
