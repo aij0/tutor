20d9c4a37c4c78ffaf3ee4c3baa165a3b7908e39
aij0 <new@email>
2021-10-16 12:45:34 +0300

    Add empty tutor component

---
+++ my-app/src/components/tutor.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./tutor.css";
+
+class Tutor extends React.Component{
+    render(){
+        return (
+            <div className="tutor">
+                <div className="tutor-picture"></div>
+                <div className="tutor-title"></div>
+            </div>
+        );
+    }
+}
+
+export default Tutor;
