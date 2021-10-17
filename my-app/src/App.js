f978e4f0ce86af43785c9c1fad3ecfd0bac91a2e
Joonas Aijala <joonas.aijala@ge.com>
2021-10-17 11:15:48 +0300

    Add google map and markers for tutors
    This would require API key from google.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Tutor from './components/tutor';
+import Marker from './components/marker';
+import GoogleMapReact from 'google-map-react';
 
 class App extends Component{
   constructor(props) {
@@ -22,6 +24,11 @@ class App extends Component{
   }
 
   render() {
+    const center = {
+      lat: 60.1699,
+      lng: 24.9384
+    }
+
     return (
       <div className="app">
         <div className="main">
@@ -34,6 +41,12 @@ class App extends Component{
           </div>
         </div>
         <div className="map">
+          <GoogleMapReact center={center} zoom={13}>
+            {this.state.tutors.map(function(tutor) {
+                  return <Marker lat={tutor.lat} lng={tutor.lng}
+                  text={tutor.price} />
+                })}  
+        </GoogleMapReact> 
         </div>
       </div>
     );
